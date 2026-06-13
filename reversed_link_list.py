f4a4295ec1e23768ae52daeb55a955fc6c8d339d
Kazi MD Shams Aryan <shamsaryan1994@gmail.com>
2026-06-13 08:10:07 +0100

    Solved on 11/6:Need more practice

--- reversed_link_list.py
+++ reversed_link_list.py
@@ -5,7 +5,13 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None:
-            return []
-        else:
-            print(head.reverse())
+        curr = head
+        prev = None
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        return prev
